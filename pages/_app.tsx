01c1ef7b7de3dc2bbce1c495a809ce7aaa14ad95
RahulBansal123 <rbbansal558@gmail.com>
2022-09-25 18:58:12 +0530

    Bug Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,10 @@ import '@rainbow-me/rainbowkit/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 import type { AppProps } from 'next/app';
-import {
-  RainbowKitProvider,
-  connectorsForWallets,
-  wallet,
-} from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
-import { sequenceWallet } from 'sequence-rainbowkit-wallet';
 import { ToastContainer } from 'react-toastify';
 
 const { chains, provider, webSocketProvider } = configureChains(
@@ -30,18 +25,10 @@ const { chains, provider, webSocketProvider } = configureChains(
   ]
 );
 
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Recommended',
-    wallets: [
-      sequenceWallet({ chains }),
-      wallet.metaMask({ chains }),
-      wallet.rainbow({ chains }),
-      wallet.walletConnect({ chains }),
-      wallet.coinbase({ chains, appName: 'zkForms' }),
-    ],
-  },
-]);
+const { connectors } = getDefaultWallets({
+  appName: 'zkForms',
+  chains,
+});
 
 const wagmiClient = createClient({
   autoConnect: true,
